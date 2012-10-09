1e33b8e636998de7ca25c93ce481e7b3b6bdeb69
nevon <tommy.brunn@gmail.com>
2012-10-10 01:18:36 +0200

    Lade till författarinformationsruta i single.php.

--- wp-content/themes/blogg-design/single.php
+++ wp-content/themes/blogg-design/single.php
@@ -29,6 +29,41 @@
 						</footer> <!-- end article footer -->
 					
 					</article> <!-- end article -->
+
+					<!-- Author info-->
+					<aside class="author_info">
+						<figure>
+							<?php echo get_avatar(get_the_author_meta('ID'), 150, 'Mystery Man', 'Avatar tillhörande '.get_the_author_meta('display_name')); ?>
+							<?php
+							$website = get_the_author_meta('user_url');
+							$googleplus = get_the_author_meta('googleplus');
+							$facebook = get_the_author_meta('user_fb');
+							$twitter = get_the_author_meta('user_tw');
+							
+							if ($user_url || $googleplus || $facebook || $twitter) {
+								echo '<ul class="author_links">';
+									if ($website) {
+										echo '<li><a href="'.$website.'"><img src="'.get_bloginfo('template_directory').'/library/images/home.png" width="18" height="18" alt="Hemsida"></a></li>';
+									}
+									if ($googleplus) {
+										echo '<li><a href="'.$googleplus.'"><img src="'.get_bloginfo('template_directory').'/library/images/gplus.png" width="18" height="18" alt="Google plus"></a></li>';
+									}
+									if ($twitter) {
+										echo '<li><a href="'.$twitter.'"><img src="'.get_bloginfo('template_directory').'/library/images/twitter.png" width="18" height="18" alt="Twitter"></a></li>';
+									}
+									if ($facebook) {
+										echo '<li><a href="'.$facebook.'"><img src="'.get_bloginfo('template_directory').'/library/images/fb.png" width="18" height="18" alt="Facebook"></a></li>';
+									}
+									
+								echo '</ul>';
+							}
+							?>
+						</figure>
+						<div>
+							<h2>Skrivet av <?php the_author_posts_link(); ?></a></h2>
+							<p><?php the_author_meta('description'); ?></p>
+						</div>
+					</aside> <!-- end author info -->
 					
 					<?php comments_template(); ?>
 					

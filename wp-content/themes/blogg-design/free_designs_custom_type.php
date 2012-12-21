9ed8d269dded8077ebed6b35f235713cbde827ef
Mimmi Thorneus <mimmi.thorneus@gmail.com>
2012-12-21 16:45:47 +0100

    Omdesign av bloggdesignsidan

--- wp-content/themes/blogg-design/free_designs_custom_type.php
+++ wp-content/themes/blogg-design/free_designs_custom_type.php
@@ -6,13 +6,13 @@ Template Name: Free Designs
 	   
 	<div id="content" class="clearfix">
 		<div id="main" class="left" role="main">
-		<div class="page_content clearfix">			
-			<h1 class="archive_title h2">Gratis bloggdesigner till blogg.se</h1>
+		<div class="page_content clearfix">				
 			
 			<?php if(have_posts()) : while (have_posts()) : the_post();
 				$content = get_the_content();
 				if (!empty($content)) {
 					print '<div class="free-designs-description">';
+					print '<h1 class="archive_title h2">Gratis bloggdesigner till blogg.se</h1>';
 					print $content;
 					print '</div>';
 				}

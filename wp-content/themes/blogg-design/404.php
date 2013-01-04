89707fcb0194d8a49a1922f2ed130189be9012a1
nevon <tommy.brunn@gmail.com>
2013-01-04 16:44:20 +0100

    Tog bort fittext.js som inte användes längre.

--- wp-content/themes/blogg-design/404.php
+++ wp-content/themes/blogg-design/404.php
@@ -29,11 +29,6 @@
 	
 				<div id="logo"><h1><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></h1></div>
 				<p class="bloginfo"><?php bloginfo('description'); ?></p>
-				
-				<script src="<?php echo get_template_directory_uri(); ?>/library/js/jquery.fittext.js"></script>
-				<script>
-					$("#logo").fitText(1.2, { minFontSize: '12px', maxFontSize: '28px' });
-				</script>
 					
 			</div>
 			
